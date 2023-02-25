f50848d29b751598b924704b68648e1273a0e15f
sheriff oladimeji <dimejiademola5@gmail.com>
2023-02-25 18:12:35 +0100

    modify files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
-import "./style.css"
+import "./style.css";
 const page = ReactDOM.createRoot(document.getElementById("root"));
 
 function Page() {
   return (
     <div>
-<App/>
+      <App />
     </div>
   );
 }
-page.render(<Page/>)
-
+page.render(<Page />);
