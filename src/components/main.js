9c07e229542ec62030785681c5fe5a5d7e2f92ed
sheriff oladimeji <dimejiademola5@gmail.com>
2023-02-25 17:29:11 +0100

    modify two files

--- src/components/main.js
+++ src/components/main.js
@@ -2,7 +2,7 @@ import React from "react";
 export default function Main() {
   return (
     <main className="container">
-      <h1>Fun facts about React</h1>
+      <h1 className="main--title">Fun facts about React</h1>
       <ul>
         <li>was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
