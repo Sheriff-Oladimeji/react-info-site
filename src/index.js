00dd857497b130941925edd376323e3890c61d42
sheriff oladimeji <dimejiademola5@gmail.com>
2023-02-25 08:08:45 +0100

    add csss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import pic from "./logo.svg";
-
+import css from "./style.css"
 const app = document.getElementById("root");
 
 function Page() {
