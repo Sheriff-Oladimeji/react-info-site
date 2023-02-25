308bc790c5a52b5b2dfb4d58a9d3ef686cfc9978
sheriff oladimeji <dimejiademola5@gmail.com>
2023-02-25 16:18:43 +0100

    delete some components

--- src/footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-export default function Footer(){
-  return (
-    <footer className="footer">
-        <small>&copy; 2023 Sheriff development. All rights reserved</small>
-      </footer>
-  )
-}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./Header";
-import MainContent from "./main";
-import Footer from "./footer";
+
 import "./style.css"
 const page = ReactDOM.createRoot(document.getElementById("root"));
 
 function App() {
   return (
     <div>
-      <Header/>
-     <MainContent/>
-     <Footer/> 
+
     </div>
   );
 }
